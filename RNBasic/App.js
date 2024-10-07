1e01feed5e3e49b292adc330b506c2c9f76dab7b
Arthur (DPA) <a.foadjo-dje@psychologenakademie.de>
2024-10-07 23:52:20 +0200

    Styling React Native component (Basics)

--- RNBasic/App.js
+++ RNBasic/App.js
@@ -4,19 +4,38 @@ export default function App() {
   return (
     <View style={styles.container}>
       <View>
-        <Text>Another piece of text</Text>
+        <Text style={styles.text}>Another piece of text</Text>
       </View>
-      <Text>Hello World!</Text>
+      <Text
+        style={styles.text}
+        // style={{
+        //   margin: 16,
+        //   borderStyle: "solid",
+        //   borderColor: "red",
+        //   borderWidth: 2,
+        //   padding: 12,
+        // }}
+      >
+        Hello World!
+      </Text>
       <Button title="Click me!" />
     </View>
   );
 }
 
+// Using a stylesheet object to improve reutilisablity
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+  },
+  text: {
+    margin: 16,
+    borderStyle: "solid",
+    borderColor: "green",
+    borderWidth: 2,
+    padding: 12,
   },
 });
