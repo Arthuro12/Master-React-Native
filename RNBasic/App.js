0a5769ce2714c16a517ec3f42b05d500c3ae3430
Arthur (DPA) <a.foadjo-dje@psychologenakademie.de>
2024-10-08 00:10:06 +0200

    App Layout

--- RNBasic/App.js
+++ RNBasic/App.js
@@ -1,41 +1,53 @@
-import { StyleSheet, Text, View, Button } from "react-native";
+import { StyleSheet, Text, View, Button, TextInput } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    // <View style={styles.container}>
+    //   <View>
+    //     <Text style={styles.text}>Another piece of text</Text>
+    //   </View>
+    //   <Text
+    //     style={styles.text}
+    //     // style={{
+    //     //   margin: 16,
+    //     //   borderStyle: "solid",
+    //     //   borderColor: "red",
+    //     //   borderWidth: 2,
+    //     //   padding: 12,
+    //     // }}
+    //   >
+    //     Hello World!
+    //   </Text>
+    //   <Button title="Click me!" />
+    // </View>
+    <View style={styles.appContainer}>
       <View>
-        <Text style={styles.text}>Another piece of text</Text>
+        <TextInput placeholder="Your course goal" />
+        <Button title="Add Goal" />
       </View>
-      <Text
-        style={styles.text}
-        // style={{
-        //   margin: 16,
-        //   borderStyle: "solid",
-        //   borderColor: "red",
-        //   borderWidth: 2,
-        //   padding: 12,
-        // }}
-      >
-        Hello World!
-      </Text>
-      <Button title="Click me!" />
+      <View>
+        <Text>List of goals</Text>
+      </View>
     </View>
   );
 }
 
 // Using a stylesheet object to improve reutilisablity
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  text: {
-    margin: 16,
-    borderStyle: "solid",
-    borderColor: "green",
-    borderWidth: 2,
-    padding: 12,
+  // container: {
+  //   flex: 1,
+  //   backgroundColor: "#fff",
+  //   alignItems: "center",
+  //   justifyContent: "center",
+  // },
+  // text: {
+  //   margin: 16,
+  //   borderStyle: "solid",
+  //   borderColor: "green",
+  //   borderWidth: 2,
+  //   padding: 12,
+  // },
+  appContainer: {
+    padding: 50,
   },
 });
