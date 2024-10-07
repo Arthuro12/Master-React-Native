91eaa28ec583d2a2327a122fcbf43b28abfb05e0
Arthur (DPA) <a.foadjo-dje@psychologenakademie.de>
2024-10-08 01:29:41 +0200

    Improvingthe layout

--- RNBasic/App.js
+++ RNBasic/App.js
@@ -25,7 +25,7 @@ export default function App() {
         <TextInput style={styles.textInput} placeholder="Your course goal" />
         <Button title="Add Goal" />
       </View>
-      <View>
+      <View style={styles.goalsContainer}>
         <Text>List of goals</Text>
       </View>
     </View>
@@ -48,17 +48,27 @@ const styles = StyleSheet.create({
   //   padding: 12,
   // },
   appContainer: {
-    padding: 50,
+    flex: 1,
+    paddingTop: 50,
+    paddingHorizontal: 16,
   },
   inputContainer: {
+    flex: 1,
     flexDirection: "row",
     justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: 24,
+    borderBottomWidth: 1,
+    borderBottomColor: "#cccccc",
   },
   textInput: {
     borderWidth: 1,
-    borderColor: "cccccc",
-    width: "80%",
+    borderColor: "#cccccc",
+    width: "70%",
     marginRight: 8,
-    paddingLeft: "2%",
+    padding: 8,
+  },
+  goalsContainer: {
+    flex: 5,
   },
 });
