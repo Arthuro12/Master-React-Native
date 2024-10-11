fc7b257883ff937ba825831647d222c44b839fa9
Arthur (DPA) <a.foadjo-dje@psychologenakademie.de>
2024-10-11 19:53:24 +0200

    Positioning the buttons and input of the StartGameScreen component

--- diving-deeper/components/PrimaryButton.js
+++ diving-deeper/components/PrimaryButton.js
@@ -1,13 +1,44 @@
-import { StyleSheet, View, Text } from "react-native";
+import { StyleSheet, View, Text, Pressable } from "react-native";
 
 function PrimaryButton({ children }) {
-    return <View>
-        <Text>{children}</Text>
-    </View>
+    function handlePress() {
+        console.log("Pressed");
+    }
+
+    return (
+       <View style={styles.buttonOuterContainer}>
+            <Pressable 
+                style={({pressed}) => 
+                    pressed 
+                        ? [styles.buttonInnerContainer, styles.pressed] 
+                        : styles.buttonInnerContainer
+                    } 
+                onPress={handlePress}
+            >
+                <Text style={styles.buttonText}>{children}</Text>
+            </Pressable>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-
+    buttonOuterContainer: {
+        borderRadius: 28,
+        margin: 4,
+        overflow: "hidden"
+    },
+    buttonInnerContainer: {
+        backgroundColor: "#72063c",
+        paddingVertical: 8,
+        paddingHorizontal: 16,
+    },
+    buttonText: {
+        color: "white",
+        textAlign: "center"
+    },
+    pressed: {
+        opacity: 0.75
+    }
 });
 
 export default PrimaryButton;
--- diving-deeper/screens/StartGameScreen.js
+++ diving-deeper/screens/StartGameScreen.js
@@ -3,28 +3,44 @@ import { StyleSheet, View, TextInput } from "react-native";
 import PrimaryButton from "../components/PrimaryButton";
 
 function StartGameScreen() {
-    return <View style={styles.inputContainer}>
-        <TextInput style={styles.input} 
-            keyboardType="number-pad" 
-            maxLength={2} 
-        />
-        <PrimaryButton>Reset</PrimaryButton>
-        <PrimaryButton>Confirm</PrimaryButton>
-    </View>
+    return (
+        <View style={styles.inputContainer}>
+            <TextInput style={styles.input} 
+                keyboardType="number-pad" 
+                maxLength={2} 
+            />
+            <View style={styles.buttons}>
+                <View style={styles.button}>
+                    <PrimaryButton>Reset</PrimaryButton>
+                </View>
+                <View style={styles.button}>
+                    <PrimaryButton>Confirm</PrimaryButton>
+                </View>
+            </View>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
     inputContainer: {
+        justifyContent: "center",
+        alignItems: "center",
         marginTop: 100,
         marginHorizontal: 24,
         borderRadius: 8,
         padding: 16,
-        backgroundColor: "#72063c",
+        backgroundColor: "#4e0329",
         elevation: 8, // Add shadow for android devices
         shadowColor: "black",
         shadowOffset: { width: 0, height: 2 },
         shadowRadius: 6,
         shadowOpacity: 0.25
+    },
+    buttons: {
+        flexDirection: "row",
+    },
+    button: {
+        flex: 1,
     },
     input: {
         width: 50,
