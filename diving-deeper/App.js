c8edad894613c3baba92a0469f74c69f53e25851
Arthur (DPA) <a.foadjo-dje@psychologenakademie.de>
2024-10-11 20:24:16 +0200

    Adding a background image using the ImageBackground component

--- diving-deeper/App.js
+++ diving-deeper/App.js
@@ -1,4 +1,4 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, ImageBackground } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
 
 import StartGameScreen from './screens/StartGameScreen';
@@ -6,7 +6,14 @@ import StartGameScreen from './screens/StartGameScreen';
 export default function App() {
   return (
     <LinearGradient colors={['#4e0329', '#ddb52f']} style={styles.rootScreen}>
-      <StartGameScreen />
+      <ImageBackground 
+        style={styles.rootScreen}
+        source={require("./assets/background.png")} 
+        imageStyle={styles.backgroundImage}
+        resizeMethod="cover"
+      >
+        <StartGameScreen />
+      </ImageBackground>
     </LinearGradient>
   );
 }
@@ -15,5 +22,8 @@ const styles = StyleSheet.create({
   rootScreen: {
     flex: 1,
     backgroundColor: "#ddb52f",
+  },
+  backgroundImage: {
+    opacity: 0.15
   }
 });
