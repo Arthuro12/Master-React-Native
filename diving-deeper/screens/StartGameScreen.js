9ce96f0f4d7c7b8ebd6f25036d311bd3290ce3d9
Arthur (DPA) <a.foadjo-dje@psychologenakademie.de>
2024-10-11 14:59:40 +0200

    Setup screens

---
+++ diving-deeper/screens/StartGameScreen.js
@@ -0,0 +1,10 @@
+import { View, TextInput } from "react-native";
+
+function StartGameScreen() {
+    return <View>
+        <TextInput />
+        {/* Buttons */}
+    </View>
+}
+
+export default StartGameScreen;
